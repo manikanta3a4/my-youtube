6fd78fefb85d764d797e788f2a1d3c30fcfe5cc4
manikanta <medamanikanta.infanion@gmail.com>
2021-02-07 14:19:28 +0530

    code for my-youtube updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Videos from './Videos';
+import VideoPlay from './VideoPlay';
+import Search from './Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <Search />
+      <div className = "col-md-7 col-lg-7 col-xs-12 col-sm-12">
+        <VideoPlay />
+      </div>
+      <div className = "col-md-5 col-lg-5 col-xs-12 col-sm-12">
+        <Videos />
+      </div>
     </div>
   );
 }
